a389a1fedb9c6b222c99493ff55a7a1871984839
shengoo <shengoo@hotmail.com>
2016-07-14 23:20:37 +0800

    init

---
+++ app/main.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by sheng on 16/7/14.
+ */
+
+
+import { bootstrap } from '@angular/platform-browser-dynamic';
+
+import { AppComponent } from './app.component';
+
+bootstrap(AppComponent);
